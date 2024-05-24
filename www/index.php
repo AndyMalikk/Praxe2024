146e8dd581444304121511487fb023377d45cd8e
Ondřej Malík <Onndrejmalik@seznam.cz>
2024-05-24 08:57:09 +0200

    Hello world, tohle je první release

---
+++ www/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "styles.css">
+    <title>Linux</title>
+</head>
+<body>
+    <!-- Navbar -->
+    <navbar>
+        <ul>
+            <li><a href = "#">Home</a></li>
+            <li><a href = "#">About Us</a></li>
+            <li><a href = "#">Distributions</a></li>
+            <li><a href = "#">History</a></li>
+            <li class = "navbar-right"><a href = "#">Contacts</a></li>
+        </ul>
+    </navbar>
+
+
+    <!-- Hero -->
+        <section id = "hero">
+            <div class="hero-image">
+                <div class="hero-text">
+                    <h1>Linux Haven</h1>
+                    <p>The best Operating System for CS</p>
+                    
+                </div>
+            </div>
+        </section>
+
+
+    <!-- AboutUs -->
+    <section id = "about-us">
+        <!-- Welcome to Linux Haven! We are passionate about the Linux operating system and dedicated to providing the latest news, tutorials, and resources for users of all levels. Whether you're a seasoned developer or a curious beginner, join our community to explore the power and freedom of Linux. -->
+        <div class="card">
+            <div><img class = "card-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Tux.svg/1200px-Tux.svg.png" alt="Linux logo"></div>
+            <div class="card-text"><p>Welcome to Linux Haven! We are passionate about the Linux operating system and dedicated to providing the latest news, tutorials, and resources for users of all levels. Whether you're a seasoned developer or a curious beginner, join our community to explore the power and freedom of Linux.</p></div>
+        </div>
+    </section>
+
+    <section id = "distributions">
+        <h2>Distributions</h2>
+        <div class = "input-container">
+            <input type="text" placeholder="Search..">
+        </div>
+    </section>
+
+    <section id = "history">
+
+    </section>
+
+
+
+    <section id = "contacts">
+
+    </section>
+
+
+</body>
+</html>
